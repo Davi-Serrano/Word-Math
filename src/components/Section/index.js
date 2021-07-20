88e86d937b633fc5251e461fc6eac95e0b98e04f
Davi Serrano <daviscardoso2901@gmail.com>
2021-07-20 04:25:06 -0300

    Renderização condicional adicionada

--- src/components/Section/index.js
+++ src/components/Section/index.js
@@ -1,3 +1,5 @@
+import { useState } from "react"
+
 import { Square } from "./Geometric/Square"
 import { Rectangle } from "./Geometric/Rectangle"
 import { Circle } from "./Geometric/Circle"
@@ -5,12 +7,45 @@ import { Triangle } from "./Geometric/Triangule"
 
 export const Section = () =>{
     
-    return(
-        <div>
-        <Square />
-        <Rectangle />
-        < Circle />
-        < Triangle />
-        </div>
-        )
+    const [ a, setA ] = useState(0)
+
+    function Proximo(){
+
+        setA(a +1)
+    }
+    function Voltar(){
+
+        setA(a - 1)
+    }
+
+    
+    if( a === 0) {
+       return (
+
+           <div>
+
+            <Square />
+            <button onClick={Proximo}> Next</button>
+            <button onClick={Voltar}> Back</button>
+       </div>
+           )
+    }
+    if( a === 1) {
+        return <Rectangle />
+ 
+     }
+     if( a === 2) {
+        return  < Circle />
+ 
+     }
+     if( a === 3) {
+        return < Triangle />
+
+ 
+     }
+               
+        
+    
+        
+        
     }
