aaeabb78cf33aafde36c623b2dd7f4931f6d1174
Davi Serrano <daviscardoso2901@gmail.com>
2021-07-28 04:06:49 -0300

    Calc Multiplicaçãp add

--- src/components/Section/Fraction/Multiplication/index.js
+++ src/components/Section/Fraction/Multiplication/index.js
@@ -1,8 +1,33 @@
+import { useState } from "react"
+
 import {Title, Account, FractionStyle, FractNumber, CalcBtn, SinalMath } from "../styled"
 
 export const Multiplication = () =>{
 
 
+    const [ Numerator, setNumerator ] = useState()
+    const [ Denominator, setDenominator ] = useState()
+
+function CalcMultiplication () {
+    
+    const numeratorA = document.querySelector("#numA").value
+    const numeratorB = document.querySelector("#numB").value
+    const denominatorA = document.querySelector("#denA").value
+    const denominatorB = document.querySelector("#denB").value
+
+    const NumA = parseInt(numeratorA)
+    const NumB = parseInt(numeratorB)
+
+    const DenA = parseInt(denominatorA)
+    const DenB = parseInt(denominatorB)
+
+
+    setNumerator(NumA * NumB)
+    setDenominator(DenA * DenB)
+    
+    
+}
+var RespNum = 0
     return(
 
             <div>
@@ -10,22 +35,22 @@ export const Multiplication = () =>{
                     <Account>
                         <FractionStyle>
                                 <FractNumber>
-                                    <input type="number" min="1" />
-                                    <input type="number" min="1" />
+                                    <input type="number" id="numA" min="1" />
+                                    <input type="number" id="denA" min="1" />
                                 </FractNumber>
                                     <SinalMath>X</SinalMath>
                                 <FractNumber>
-                                    <input type="number" min="1" />
-                                    <input type="number" min="1" />
+                                    <input type="number" id="numB" min="1"  />
+                                    <input type="number" id="denB" min="1" />
                                 </FractNumber>
                                 <SinalMath>=</SinalMath>
-                                <FractNumber>
-                                    <input type="number" min="1" />
-                                    <input type="number" min="1" />
+                                <FractNumber>/
+                                    <input  value={Numerator}  />
+                                    <input  value={Denominator}  />
                                 </FractNumber>
 
                     </FractionStyle>
-                                <CalcBtn>Calcular</CalcBtn>
+                                <CalcBtn onClick={CalcMultiplication}>Calcular</CalcBtn>
                     </Account>
 
             </div>
